44c6f021b83df37ab49a9a9871930e8da49ebaae
Rohtash Lahry <rohtashlahry@gmail.com>
2024-12-28 19:03:45 +0530

    next app pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: withPWA({
+    dest: "public", // The directory for service worker files
+    register: true, // Automatically register the service worker
+    skipWaiting: true, // Activate the new service worker immediately
+  }),
 };
 
 export default nextConfig;
